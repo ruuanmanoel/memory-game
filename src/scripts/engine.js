ebb0b7e66f859260d24105c936ebc909e398ea7e
Ruan Manoel <ruuanmanoel@gmail.com>
2024-10-25 00:54:30 -0300

    ✨ aplicando rotate nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,12 +19,26 @@ const emojis = [
 let openCards = [];
 
 let shuffleEmoji = emojis.sort(()=>{
-    Math.random()>0.5 ? 2 :-1;
+    return Math.random()>0.5 ? 2 : -1;
 })
 
 for(let i = 0; i <emojis.length; i++){
     let box = document.createElement('div');
     box.className = "item";
     box.innerHTML = shuffleEmoji[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add('boxOpen');
+        openCards.push(this);
+    }
+    if(openCards.length == 2){
+        setTimeout(checkMatch,500);
+    }
+}
+function checkMatch(){
+    
 }
